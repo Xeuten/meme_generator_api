fc405abf4655b7a820662ec6d404882186008463
Xeuten <xeuten@gmail.com>
2024-10-07 17:48:13 +0300

    added one more comment

--- meme_generator_api/urls.py
+++ meme_generator_api/urls.py
@@ -25,5 +25,7 @@ urlpatterns = [
     path("api/", include("api.urls")),
 ]
 
+# In a production environment, we would've used another approach to serve images for
+# the surprise-me feature.
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
